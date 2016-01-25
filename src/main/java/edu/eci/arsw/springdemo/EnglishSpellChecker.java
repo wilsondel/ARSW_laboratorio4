2e665fba79ca8d56e92012bcb7c393ee4d688aff
Hector <hector.cadavid@gmail.com>
2016-01-25 17:06:25 -0500

    initial commit

---
+++ src/main/java/edu/eci/arsw/springdemo/EnglishSpellChecker.java
@@ -0,0 +1,10 @@
+package edu.eci.arsw.springdemo;
+
+public class EnglishSpellChecker implements SpellChecker {
+
+	@Override
+	public String checkSpell(String text) {		
+		return "Checked with english checker:"+text;
+	}
+
+}
---
+++ src/main/java/edu/eci/arsw/springdemo/SpanishSpellChecker.java
@@ -0,0 +1,10 @@
+package edu.eci.arsw.springdemo;
+
+public class SpanishSpellChecker implements SpellChecker {
+
+	@Override
+	public String checkSpell(String text) {
+		return "revisando ("+text+") con el verificador de sintaxis del espanol";
+	}
+
+}
